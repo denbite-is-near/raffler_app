ecb396601a5e1b5a5163faf158dea64fbbbe92d6
Den <mirloxd3@gmail.com>
2022-07-30 14:27:12 +0300

    fix: env

--- ui/config/index.ts
+++ ui/config/index.ts
@@ -1,2 +1,2 @@
 export const nearNetwork = process.env.NEXT_PUBLIC_NEAR_NETWORK || "testnet";
-export const contractId = "dev-1657963981044-32074586353391";
+export const contractId = process.env.NEXT_PUBLIC_RAFFLER_CONTRACT_ADDRESS;
