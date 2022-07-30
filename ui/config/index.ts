41841422095a69730619dfd246cf02393bd5c966
Den <mirloxd3@gmail.com>
2022-07-30 14:32:43 +0300

    fix: minor

--- ui/config/index.ts
+++ ui/config/index.ts
@@ -1,2 +1,3 @@
 export const nearNetwork = process.env.NEXT_PUBLIC_NEAR_NETWORK || "testnet";
-export const contractId = process.env.NEXT_PUBLIC_RAFFLER_CONTRACT_ADDRESS;
+export const contractId =
+  process.env.NEXT_PUBLIC_RAFFLER_CONTRACT_ADDRESS || "";
